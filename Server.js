14ac4aaa9dc52fbb33d25f7287a202f3271dac77
Amrit Anand <amrit.anand@s759labs.com>
2022-05-27 11:50:26 +0530

    changed in package.json

--- Server.js
+++ Server.js
@@ -6,8 +6,15 @@ const server = http.createServer(app);
 const io = new Server(server);
 const PORT = process.env.POST || 5000;
 const ACTIONS = require('./src/Actions');
+const path = require("path");
+
+app.use(express.static('build'));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 const userSocketMap = {};
+
 
 function getAllConnectedClients(roomId){
   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
