d210c1c1befceb3c923fd0e5a54cd4c57e1a7b61
Amrit Anand <amrit.anand@s759labs.com>
2022-05-27 12:53:48 +0530

    added PORT

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const {Server} = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-const PORT = process.env.POST || 5000;
+const PORT = process.env.PORT || 5000;
 const ACTIONS = require('./src/Actions');
 const path = require("path");
 
